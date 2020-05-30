ce8fe3b960decfed5de063eda5f224dffdc2b91b
Dennis Harrop <dh450@exeter.ac.uk>
2020-05-30 16:43:57 +0100

    get web page

--- scrape.py
+++ scrape.py
@@ -5,5 +5,17 @@ import re
 import csv
 
 
-def scrap_thoughts(url):
-    html = BeautifulSoup()
+def scrape_thoughts(url):
+    req = urllib.request.urlopen(url)
+    page = req.read().decode('utf-8')
+    soup = BeautifulSoup(page,'html.parser')
+    
+    trans = soup.find(id='mw-content-text')
+    ps = trans.find_all('p')
+   
+    title = ps[0].find('b').get_text()
+    print(title)
+    #  for p in ps:
+        #  print (p)
+url = "https://transcripts.fandom.com/wiki/University_Challenge"
+scrape_thoughts(url)
